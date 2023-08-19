30ab2cee18d4f70923022e4fd0939b3b8f7ac009
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-08-20 00:24:41 +0500

    Added JS files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { DateTime } from '../node_modules/luxon/src/luxon.js';
+import BookList from '../modules/bookList.js';
+
+const bookList = new BookList();
+bookList.init();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const timer = document.getElementById('datetime');
+  const clock = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+  timer.textContent = clock;
+});
